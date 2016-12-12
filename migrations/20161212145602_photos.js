758010112cf27d8ed3387f01c8ad73c4078e2adb
Heidi Rodriguez <hrod2645@gmail.com>
2016-12-12 15:23:37 -0700

    photo migration on master

---
+++ migrations/20161212145602_photos.js
@@ -0,0 +1,14 @@
+'use strict';
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('photos', (table) => {
+    table.increments();
+    table.integer('towns_id').notNullable().references('id').inTable('towns').onDelete('cascade');
+    table.integer('users_id').defaultTo().references('id').inTable('users').onDelete('cascade');
+    table.text('photo_url').notNullable().defaultTo('No photo available');
+
+  });
+};
+
+exports.down = function(knex, Promise) {
+return knex.schema.dropTable('photos');
+};
