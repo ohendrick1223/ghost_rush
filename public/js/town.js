021e94bf8326ded92081e23df1a7f42caa4649fc
KatieAJenkins <kind2karma@gmail.com>
2016-12-13 16:14:16 -0700

    additional map progress

--- public/js/town.js
+++ public/js/town.js
@@ -13,8 +13,7 @@ function init() {
     center: tarryall
   } );
 
-//document ready
-(function() {
+  $('document').ready(function() {
 
 console.log("I'm ready!");
 
@@ -25,8 +24,7 @@ console.log("I'm ready!");
   const townID = urlParams.get('id');
 
   const townAPIroute = '/towns/' + townID;
-
-  $('document').ready(function() {
+  console.log("routes ready!");
 
     $.getJSON(townAPIroute)
     .done((town) => {
@@ -40,41 +38,45 @@ console.log("I'm ready!");
     .fail(function(err) {
       console.log(err);
     });
-});
+// });
 
-  function Town (town) {
-   const obj = town;
-   this.id = obj[ "id" ];
-   this.townPhoto = obj[ "photo_url" ];
-   this.townName = obj[ "name" ];
-   this.latlon = (parseFloat( obj[ "latitude" ] ) + ", " + parseFloat( obj[ "longitude" ] )).toString()
+  function TownLocation(townObj) {
+    const obj = townObj;
+    this.id = obj["id"];
+    this.name = obj["name"];
+    // this.photoUrl = obj["photo_url"];
+    // this.shortDescription = obj["short_desc"];
+    this.lat = parseFloat(obj["latitude"]);
+    this.lng = parseFloat(obj["longitude"]);
+  }
 
-   this.townCemetery = obj [ "cemetery" ];
-   this.townEst = obj ["yr_est"];
-   this.townAbandon = obj ["yr_abnd"];
-   this.townMaterials = obj ["mineral_found"];
-   this.townTour = obj ["tour_avail"];
-   this.townDescThen = obj ["description_then"];
-   this.townDescNow = obj ["description_now"];
-   }
+  TownLocation.prototype.position = function() {
+    var newLat = {
+        lat: this.lat
+    };
+    var newLng = {
+        lng: this.lng
+    };
+    return Object.assign({}, newLat, newLng);
+  };
 
-   Town.prototype.populateTown  = function populateTown () {
-     //function to render town
-     $('#image').attr({ src: this.townPhoto, alt: this.townName });
-     $('#townName').text(this.townName);
-     $('#townLocation').text(this.latlon);
-     $('#townCemetery').text(this.townCemetery);
-     $('#townEst').text(this.townEst);
-     $('#townAbandon').text(this.townAbandon);
-     $('#townMaterials').text(this.townMaterials);
-     $('#townTour').text(this.townTour);
-     $('#townDescThen').text(this.townDescThen);
-     $('#townDescNow').text(this.townDescNow);
-   };
-
-//town map
-// function init() {
-//   var
-// }
-})();
+  //render markers
+  function renderMarker(obj) {
+    var newTown = new TownLocation(obj);
+    // var content = newTown.contentString();
+    var position = newTown.position();
+    // var name = newTown.name;
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: name
+    });
+    // var infowindow = new google.maps.InfoWindow({
+    //     content: content
+    // });
+    // marker.addListener('click', function() {
+    //     infowindow.open(map, marker);
+    // });
+    }
+  }
 }
